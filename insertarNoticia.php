96e8f66df20ca2f12d4247078c18c6a8638b7f21
Pablo <dbn_00@hotmail.com>
2015-03-07 21:17:39 +0100

    Reduccion de codigo

--- insertarNoticia.php
+++ insertarNoticia.php
@@ -1,55 +1,63 @@
 <?php
 /*require_once('accesoseguro.php');*/
-require_once('cnxBD.php');
+require_once('../cnxBD.php');
+$msg =" ";
+$recupera =" ";
 
-if(isset($_POST['enviar'])){ 
-		if(mb_strlen($_POST['titulo'])>=15){
-			$tituloBien=true;
-				
-		}else{
-				$tituloBien=false;
-				$error1 = " TIENES QUE PONER MAS LETRAS";
-		}
-	if(mb_strlen($_POST['noticia'])>=80){
-		$noticiaBien=true;
-	}else{
-		$noticiaBien=false;
-		$error2= "UTILIZA UN MINIMO DE 80 CARACTERES";
-	}
+	if(isset($_POST['enviar'])){ 
+			
+			if (mb_strlen($_POST['txtTitulo'])<=120){
+				$titulo = $_POST['txtTitulo'];
+				$noticia = $_POST['txtNoticia'];
+				$fecha = time();
 
-	if($noticiaBien && $tituloBien){
-		$idcon=mysql_connect('localhost','root','');
-		mysql_select_db('WM1',$idcon);
-		$titulo = $_POST['titulo'];
-		$noticia = $_POST['noticia'];
-		$fecha = time();
-		$sql="INSERT INTO blogNoticias (Titulo,Noticia,Fecha) VALUES ('$titulo','$noticia',$fecha)"; //campos varchar entre comillas, campos int como la fecha sin comillas
-		if(mysql_query($sql,$idcon)){
-			$msg="El formulario ha sido enviado";
-		}else{ 
-				$msg="Todos los campos son obligatorios";
-		}
+			
+				$sql="INSERT INTO blogNoticias (Titulo,Noticia,Fecha) VALUES ('$titulo','$noticia',$fecha)"; //campos varchar entre comillas, campos int como la fecha sin comillas
+				if(mysql_query($sql,$idcon)){
+					$msg="Noticia Insertada";
+				}else{ 
+					$msg="Todos los campos son obligatorios";
+				}
+			}else{
+				$msg="Maximo 120 caracteres en el Titulo";
+				$recupera = $_POST['txtNoticia'];
+
+			}
+			
 
 	}
-
-}
 ?>
 
-?>
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine:600,700,900" media="all">
-	<link type="text/css" rel="stylesheet" href="style.css">
+	<link type="text/css" rel="stylesheet" href="../style.css">
 </head>
 
 <body>
-	<?php include ('cabeceraAdmin.html');?>
+	<?php include ('../cabeceraAdmin.html');?>
 	
 	<div id="cuerpo">
 		<div id="contenidoCuerpo">
+			<div class="testbox">
+			  <h1>Insertar Noticia</h1>
 
+				  <form action="insertarNoticia.php" method="post" enctype="multipart/form-data" name="altaRazas" id="altaRazas">
+
+					  <input type="text" name="txtTitulo" id="txtTitulo" placeholder="Titulo" required />
+					  <textarea  name="txtNoticia" id="txtNoticia" required cols="50" rows="20"> <?php echo $recupera ?> </textarea> 
+					 <br/><br/>
+					 <p><?php echo $msg;?></p>
+					
+					<input type="submit" name="enviar" id="enviar" value="Insertar" class="button">
+
+				  </form>
+			</div>			
 		</div>
 	</div><!--FIN CUERPO-->
 </body>
